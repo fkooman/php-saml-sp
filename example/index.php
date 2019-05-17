12c8a899b7eb4bb81a94fbc6c1031b3ff219f6d2
François Kooman <fkooman@tuxed.net>
2019-05-17 23:33:01 +0200

    update example

--- example/index.php
+++ example/index.php
@@ -35,6 +35,8 @@ try {
 
     $idpInfoSource = new XmlIdpInfoSource(__DIR__.'/x509idp.moonshot.utr.surfcloud.nl.xml');
     $idpEntityId = 'https://x509idp.moonshot.utr.surfcloud.nl/metadata';
+    //$idpInfoSource = new XmlIdpInfoSource(__DIR__.'/localhost.xml');
+    //$idpEntityId = 'http://localhost:8080/metadata';
     $relayState = 'http://localhost:8081/';
 
     // configure the SP
