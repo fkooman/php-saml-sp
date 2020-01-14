0ce8c97506c404c353ce7f50bcc1e37070ab0805
François Kooman <fkooman@tuxed.net>
2020-01-14 23:42:03 +0100

    some refactoring, attempt at fixing external disco service

--- src/Web/Service.php
+++ src/Web/Service.php
@@ -51,139 +51,14 @@ class Service
     public function run(Request $request)
     {
         try {
-            switch ($request->getPathInfo()) {
-                // landing page
-                case '/':
-                    return new HtmlResponse(
-                        $this->tpl->render(
-                            'index',
-                            [
-                                'returnTo' => $request->getRootUri().'info',
-                                'metadataUrl' => $request->getRootUri().'metadata',
-                                'samlMetadata' => $this->sp->metadata(),
-                            ]
-                        )
-                    );
-
-                case '/info':
-                    if (!$this->sp->hasAssertion()) {
-                        return new RedirectResponse($request->getRootUri().'wayf?ReturnTo='.$request->getUri());
-                    }
-
-                    return new HtmlResponse(
-                        $this->tpl->render(
-                            'info',
-                            [
-                                'returnTo' => $request->getRootUri(),
-                                'samlAssertion' => $this->sp->getAssertion(),
-                            ]
-                        )
-                    );
-
-                case '/wayf':
-                    // get the list of IdPs that can be used
-                    if (null === $availableIdpList = $this->config->requireKey('idpList')) {
-                        $availableIdpList = [];
-                    }
-
-                    if (null === $idpEntityId = $request->optionalQueryParameter('IdP')) {
-                        // check whether we have exactly 1 configured IdP, then use
-                        // that one!
-                        if (1 === \count($availableIdpList)) {
-                            $idpEntityId = $availableIdpList[0];
-                        }
-                    }
-
-                    // XXX validate ReturnTo... should it at least match Origin?!
-                    $returnTo = $request->requireQueryParameter('ReturnTo');
-
-                    if (null === $idpEntityId) {
-                        // we still don't know which IdP to choose, so allow user
-                        // to choose
-
-                        //                    // if a discovery service is specified, use that one
-                        //                    if (null !== $discoUrl = $this->config->requireKey('discoUrl')) {
-                        //                        // XXX maybe we should make ReturnTo required? now this is a bit crazy...
-                        //                        $returnUrl = \sprintf('%s?%s', $request->getRootUri().'login', \http_build_query(['ReturnTo' => $returnTo]));
-                        //                        $discoQuery = \http_build_query(
-                        //                            [
-                        //                                'entityID' => $this->sp->getSpInfo()->getEntityId(),
-                        //                                'returnIDParam' => 'IdP',
-                        //                                'return' => $returnUrl,
-                        //                            ]
-                        //                        );
-
-                        //                        $querySeparator = false === \strpos($discoUrl, '?') ? '?' : '&';
-
-                        //                        return new RedirectResponse(
-                        //                            \sprintf(
-                        //                                '%s%s%s',
-                        //                                $discoUrl,
-                        //                                $querySeparator,
-                        //                                $discoQuery
-                        //                            )
-                        //                        );
-                        //                    }
-
-                        $idpInfoList = [];
-                        foreach ($availableIdpList as $availableIdp) {
-                            if ($this->sp->getIdpInfoSource()->has($availableIdp)) {
-                                $idpInfoList[] = $this->sp->getIdpInfoSource()->get($availableIdp);
-                            }
-                        }
-
-                        // otherwise, show them a simple "WAYF"
-                        return new HtmlResponse(
-                            $this->tpl->render(
-                                'wayf',
-                                [
-                                    'returnTo' => $returnTo,
-                                    'idpInfoList' => $idpInfoList,
-                                ]
-                            )
-                        );
-                    }
-
-                    // make sure the provided IdP exists
-                    // XXX does sp->login take care of this as well?!
-                    if (!\in_array($idpEntityId, $availableIdpList, true)) {
-                        throw new HttpException(400, 'IdP does not exist');
-                    }
-
-                    // we know which IdP to go to!
-                    return new RedirectResponse($this->sp->login($idpEntityId, $returnTo));
-
-                // user triggered logout
-                case '/logout':
-                    // XXX validate ReturnTo... should it at least match Origin?!
-                    $returnTo = $request->requireQueryParameter('ReturnTo');
-
-                    return new RedirectResponse($this->sp->logout($returnTo));
-
-                // callback from IdP containing the "SAMLResponse"
-                case '/acs':
-                    if ('POST' === $request->getRequestMethod()) {
-                        // listen only for POST HTTP request
-                        $returnTo = $this->sp->handleResponse($request->requirePostParameter('SAMLResponse'), $request->requirePostParameter('RelayState'));
-
-                        return new RedirectResponse($returnTo);
-                    }
-
-                    throw new HttpException(405, '', ['Allow' => 'POST']);
-                // exposes the SP metadata for IdP consumption
-                case '/metadata':
-                    return new Response(200, ['Content-Type' => 'application/samlmetadata+xml'], $this->sp->metadata());
-
-                // callback from IdP containing the "LogoutResponse"
-                case '/slo':
-                    // we need the "raw" query string to be able to verify the
-                    // signatures...
-                    $returnTo = $this->sp->handleLogoutResponse($request->getQueryString());
-
-                    return new RedirectResponse($returnTo);
-
+            switch ($request->getRequestMethod()) {
+                case 'HEAD':
+                case 'GET':
+                    return $this->runGet($request);
+                case 'POST':
+                    return $this->runPost($request);
                 default:
-                    throw new HttpException(404);
+                    throw new HttpException(405, 'Only HTTP POST allowed', ['Allow' => 'GET,HEAD,POST']);
             }
         } catch (HttpException $e) {
             return new HtmlResponse(
@@ -195,6 +70,141 @@ class Service
                 ),
                 (int) $e->getCode()
             );
+        }
+    }
+
+    /**
+     * @return Response
+     */
+    public function runGet(Request $request)
+    {
+        switch ($request->getPathInfo()) {
+            // landing page
+            case '/':
+                return new HtmlResponse(
+                    $this->tpl->render(
+                        'index',
+                        [
+                            'returnTo' => $request->getRootUri().'info',
+                            'metadataUrl' => $request->getRootUri().'metadata',
+                            'samlMetadata' => $this->sp->metadata(),
+                        ]
+                    )
+                );
+            case '/info':
+                if (!$this->sp->hasAssertion()) {
+                    return new RedirectResponse($request->getRootUri().'wayf?ReturnTo='.$request->getUri());
+                }
+
+                return new HtmlResponse(
+                    $this->tpl->render(
+                        'info',
+                        [
+                            'returnTo' => $request->getRootUri(),
+                            'samlAssertion' => $this->sp->getAssertion(),
+                        ]
+                    )
+                );
+            case '/wayf':
+                // get the list of IdPs that can be used
+                if (null === $availableIdpList = $this->config->requireKey('idpList')) {
+                    $availableIdpList = [];
+                }
+
+                if (null === $idpEntityId = $request->optionalQueryParameter('IdP')) {
+                    // check whether we have exactly 1 configured IdP, then use
+                    // that one!
+                    if (1 === \count($availableIdpList)) {
+                        $idpEntityId = $availableIdpList[0];
+                    }
+                }
+
+                // XXX validate ReturnTo... should it at least match Origin?!
+                $returnTo = $request->requireQueryParameter('ReturnTo');
+
+                if (null === $idpEntityId) {
+                    // we don't know the IdP (yet)
+                    if (null !== $discoUrl = $this->config->requireKey('discoUrl')) {
+                        // use external discovery service
+                        $discoQuery = \http_build_query(
+                            [
+                                'entityID' => $this->sp->getSpInfo()->getEntityId(),
+                                'returnIDParam' => 'IdP',
+                                'return' => $request->getUri(),
+                            ]
+                        );
+
+                        return new RedirectResponse(
+                            \sprintf(
+                                '%s%s%s',
+                                $discoUrl,
+                                false === \strpos($discoUrl, '?') ? '?' : '&',
+                                $discoQuery
+                            )
+                        );
+                    }
+
+                    // we we use our own built-in simple "WAYF"
+                    $idpInfoList = [];
+                    foreach ($availableIdpList as $availableIdp) {
+                        if ($this->sp->getIdpInfoSource()->has($availableIdp)) {
+                            $idpInfoList[] = $this->sp->getIdpInfoSource()->get($availableIdp);
+                        }
+                    }
+
+                    return new HtmlResponse(
+                        $this->tpl->render(
+                            'wayf',
+                            [
+                                'returnTo' => $returnTo,
+                                'idpInfoList' => $idpInfoList,
+                            ]
+                        )
+                    );
+                }
+
+                // make sure the provided IdP exists
+                // XXX does sp->login take care of this as well?!
+                if (!\in_array($idpEntityId, $availableIdpList, true)) {
+                    throw new HttpException(400, 'IdP does not exist');
+                }
+
+                // we know which IdP to go to!
+                return new RedirectResponse($this->sp->login($idpEntityId, $returnTo));
+            // user triggered logout
+            case '/logout':
+                // XXX validate ReturnTo... should it at least match Origin?!
+                $returnTo = $request->requireQueryParameter('ReturnTo');
+
+                return new RedirectResponse($this->sp->logout($returnTo));
+            // exposes the SP metadata for IdP consumption
+            case '/metadata':
+                return new Response(200, ['Content-Type' => 'application/samlmetadata+xml'], $this->sp->metadata());
+            // callback from IdP containing the "LogoutResponse"
+            case '/slo':
+                // we need the "raw" query string to be able to verify the
+                // signatures...
+                $returnTo = $this->sp->handleLogoutResponse($request->getQueryString());
+
+                return new RedirectResponse($returnTo);
+            default:
+                throw new HttpException(404, 'URL not found: '.$request->getPathInfo());
+        }
+    }
+
+    /**
+     * @return Response
+     */
+    public function runPost(Request $request)
+    {
+        switch ($request->getPathInfo()) {
+            // callback from IdP containing the "SAMLResponse"
+            case '/acs':
+                $returnTo = $this->sp->handleResponse($request->requirePostParameter('SAMLResponse'), $request->requirePostParameter('RelayState'));
+
+                return new RedirectResponse($returnTo);
+            default:
+                throw new HttpException(404, 'URL not found: '.$request->getPathInfo());
         }
     }
 }
